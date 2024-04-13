a7a8c569afedde3c8b2b8b0bc60b9d9c3d48e534
Karlito16 <karlo.dimjasevic@gmail.com>
2024-04-13 21:04:51 +0200

    Word cache cleanup. Class diagram added. SQL updated. Initial docs file added.

--- docs/DZ02/NeighbourConnect-KreiranjeBaze.sql
+++ docs/DZ02/NeighbourConnect-KreiranjeBaze.sql
@@ -1,216 +1,205 @@
 CREATE TABLE "Država" (
-  "id" integer PRIMARY KEY,
-  "ime" varchar(50) UNIQUE NOT NULL,
-  "kratica" varchar(3) UNIQUE NOT NULL,
-  "metaId" integer NOT NULL
+                          "id" integer PRIMARY KEY,
+                          "ime" varchar(50) UNIQUE NOT NULL,
+                          "kratica" varchar(3) UNIQUE NOT NULL,
+                          "metaId" integer UNIQUE NOT NULL
 );
 
 CREATE TABLE "Mjesto" (
-  "id" integer PRIMARY KEY,
-  "državaId" integer NOT NULL,
-  "ime" varchar(50) UNIQUE NOT NULL,
-  "postanskiBroj" integer UNIQUE NOT NULL,
-  "metaId" integer NOT NULL
+                          "id" integer PRIMARY KEY,
+                          "državaId" integer NOT NULL,
+                          "ime" varchar(50) UNIQUE NOT NULL,
+                          "postanskiBroj" integer UNIQUE NOT NULL,
+                          "metaId" integer UNIQUE NOT NULL
 );
 
 CREATE TABLE "Ulica" (
-  "id" integer PRIMARY KEY,
-  "mjestoId" integer NOT NULL,
-  "ime" varchar(50) UNIQUE NOT NULL,
-  "metaId" integer NOT NULL
+                         "id" integer PRIMARY KEY,
+                         "mjestoId" integer NOT NULL,
+                         "ime" varchar(50) UNIQUE NOT NULL,
+                         "metaId" integer UNIQUE NOT NULL
 );
 
 CREATE TABLE "Ulaz" (
-  "id" integer PRIMARY KEY,
-  "zgradaId" integer NOT NULL,
-  "predstavnikId" integer,
-  "ulicaId" integer NOT NULL,
-  "ulicaBroj" integer NOT NULL,
-  "metaId" integer NOT NULL
+                        "id" integer PRIMARY KEY,
+                        "zgradaId" integer NOT NULL,
+                        "predstavnikId" integer,
+                        "ulicaId" integer NOT NULL,
+                        "ulicaBroj" integer NOT NULL,
+                        "metaId" integer UNIQUE NOT NULL,
+                        UNIQUE ("ulicaId", "ulicaBroj")
 );
 
 CREATE TABLE "Zgrada" (
-  "id" integer PRIMARY KEY,
-  "datumPočetkaGradnje" date NOT NULL,
-  "datumIzgradnje" date NOT NULL,
-  "metaId" integer NOT NULL
+                          "id" integer PRIMARY KEY,
+                          "datumPočetkaGradnje" date NOT NULL,
+                          "datumIzgradnje" date NOT NULL,
+                          "metaId" integer UNIQUE NOT NULL
 );
 
 CREATE TABLE "Ugovor" (
-  "id" integer PRIMARY KEY,
-  "zgradaId" integer NOT NULL,
-  "kompanijaId" integer NOT NULL,
-  "datumSklapanja" date NOT NULL,
-  "datumIsteka" date NOT NULL,
-  "metaId" integer NOT NULL
+                          "id" integer PRIMARY KEY,
+                          "zgradaId" integer NOT NULL,
+                          "kompanijaId" integer NOT NULL,
+                          "datumSklapanja" date NOT NULL,
+                          "datumIsteka" date NOT NULL,
+                          "metaId" integer UNIQUE NOT NULL
 );
 
 CREATE TABLE "Kompanija" (
-  "id" integer PRIMARY KEY,
-  "vlasnikId" integer NOT NULL,
-  "ulicaId" integer NOT NULL,
-  "ime" varchar(50) NOT NULL,
-  "oib" varchar(11) UNIQUE NOT NULL,
-  "godinaOsnutka" date,
-  "ulicaBroj" integer NOT NULL,
-  "metaId" integer NOT NULL
+                             "id" integer PRIMARY KEY,
+                             "vlasnikId" integer NOT NULL,
+                             "ulicaId" integer NOT NULL,
+                             "ime" varchar(50) NOT NULL,
+                             "oib" varchar(11) UNIQUE NOT NULL,
+                             "godinaOsnutka" date,
+                             "ulicaBroj" integer NOT NULL,
+                             "metaId" integer UNIQUE NOT NULL,
+                             UNIQUE ("ulicaId", "ulicaBroj")
 );
 
 CREATE TABLE "Korisnik" (
-  "id" integer PRIMARY KEY,
-  "ime" varchar(50) NOT NULL,
-  "prezime" varchar(50) NOT NULL,
-  "oib" varchar(11) NOT NULL,
-  "datumRodenja" date,
-  "mjestoRodenjaId" integer,
-  "mjestoPrebivalištaId" integer,
-  "email" varchar(50) UNIQUE NOT NULL,
-  "lozinka" varchar(200) NOT NULL,
-  "metaId" integer NOT NULL
+                            "id" integer PRIMARY KEY,
+                            "ime" varchar(50) NOT NULL,
+                            "prezime" varchar(50) NOT NULL,
+                            "oib" varchar(11) NOT NULL,
+                            "datumRodenja" date,
+                            "mjestoRodenjaId" integer,
+                            "mjestoPrebivalištaId" integer,
+                            "email" varchar(50) UNIQUE NOT NULL,
+                            "lozinka" varchar(200) NOT NULL,
+                            "metaId" integer UNIQUE NOT NULL
 );
 
 CREATE TABLE "Upravitelj" (
-  "id" integer PRIMARY KEY,
-  "datumZaposlenja" date
+                              "id" integer PRIMARY KEY,
+                              "datumZaposlenja" date
 );
 
 CREATE TABLE "Stanar" (
-  "id" integer PRIMARY KEY,
-  "pristajeBitiPredstavnik" bool NOT NULL DEFAULT true
+                          "id" integer PRIMARY KEY,
+                          "pristajeBitiPredstavnik" bool NOT NULL DEFAULT true
 );
 
 CREATE TABLE "Kontakt" (
-  "id" integer PRIMARY KEY,
-  "korisnikId" integer NOT NULL,
-  "vrstaKontaktId" integer NOT NULL,
-  "kontakt" varchar(50) NOT NULL,
-  "primarni" bool NOT NULL DEFAULT true,
-  "metaId" integer NOT NULL
+                           "id" integer PRIMARY KEY,
+                           "korisnikId" integer NOT NULL,
+                           "vrstaKontaktId" integer NOT NULL,
+                           "kontakt" varchar(50) NOT NULL,
+                           "primarni" bool NOT NULL DEFAULT true,
+                           "metaId" integer UNIQUE NOT NULL
 );
 
 CREATE TABLE "VrstaKontakt" (
-  "id" integer PRIMARY KEY,
-  "nazivVrste" varchar(50) UNIQUE NOT NULL,
-  "metaId" integer NOT NULL
+                                "id" integer PRIMARY KEY,
+                                "nazivVrste" varchar(50) UNIQUE NOT NULL,
+                                "metaId" integer UNIQUE NOT NULL
 );
 
 CREATE TABLE "NalogPlacanja" (
-  "id" integer PRIMARY KEY,
-  "zgradaId" integer NOT NULL,
-  "datumIzdavanja" date,
-  "rokUplate" date NOT NULL,
-  "zaMjesec" date NOT NULL,
-  "iznos" decimal NOT NULL,
-  "opis" varchar(200),
-  "metaId" integer NOT NULL
+                                 "id" integer PRIMARY KEY,
+                                 "zgradaId" integer NOT NULL,
+                                 "datumIzdavanja" date,
+                                 "rokUplate" date NOT NULL,
+                                 "zaMjesec" date NOT NULL,
+                                 "iznos" decimal NOT NULL,
+                                 "opis" varchar(200),
+                                 "metaId" integer UNIQUE NOT NULL
 );
 
 CREATE TABLE "Pričuva" (
-  "id" integer PRIMARY KEY,
-  "nalogPlacanjaId" integer NOT NULL,
-  "kucanstvoId" integer NOT NULL,
-  "postotakIznos" float NOT NULL DEFAULT 1,
-  "datumPlacanja" datetime,
-  "metaId" integer NOT NULL
+                           "id" integer PRIMARY KEY,
+                           "nalogPlacanjaId" integer NOT NULL,
+                           "kucanstvoId" integer NOT NULL,
+                           "postotakIznos" decimal NOT NULL DEFAULT 1,
+                           "datumPlacanja" timestamp,
+                           "metaId" integer UNIQUE NOT NULL
 );
 
 CREATE TABLE "Kućanstvo" (
-  "id" integer PRIMARY KEY,
-  "ulazId" integer NOT NULL,
-  "stanarId" integer,
-  "napomena" varchar(200),
-  "metaId" integer NOT NULL
+                             "id" integer PRIMARY KEY,
+                             "ulazId" integer NOT NULL,
+                             "stanarId" integer,
+                             "napomena" varchar(200),
+                             "metaId" integer UNIQUE NOT NULL
 );
 
 CREATE TABLE "Pritužba" (
-  "id" integer PRIMARY KEY,
-  "kućanstvoId" integer NOT NULL,
-  "naslov" varchar(50) NOT NULL,
-  "opis" varchar(200) NOT NULL,
-  "privatna" bool NOT NULL DEFAULT true,
-  "obavijestiPredstavnika" bool NOT NULL DEFAULT false,
-  "metaId" integer NOT NULL
+                            "id" integer PRIMARY KEY,
+                            "kućanstvoId" integer NOT NULL,
+                            "naslov" varchar(50) NOT NULL,
+                            "opis" varchar(200) NOT NULL,
+                            "privatna" bool NOT NULL DEFAULT true,
+                            "obavijestiPredstavnika" bool NOT NULL DEFAULT false,
+                            "metaId" integer UNIQUE NOT NULL
 );
 
 CREATE TABLE "PrijavljenaKućanstva" (
-  "kućanstvoid" integer,
-  "pritužbaId" integer,
-  "napomena" varchar(200),
-  PRIMARY KEY ("kućanstvoid", "pritužbaId")
+                                        "kućanstvoid" integer,
+                                        "pritužbaId" integer,
+                                        "napomena" varchar(200),
+                                        PRIMARY KEY ("kućanstvoid", "pritužbaId")
 );
 
 CREATE TABLE "Oglas" (
-  "id" integer PRIMARY KEY,
-  "naslov" varchar(50) NOT NULL,
-  "opis" varchar(200) NOT NULL,
-  "cijena" decimal NOT NULL
+                         "id" integer PRIMARY KEY,
+                         "naslov" varchar(50) NOT NULL,
+                         "opis" varchar(200) NOT NULL,
+                         "cijena" decimal NOT NULL,
+                         "metaId" integer UNIQUE NOT NULL
 );
 
 CREATE TABLE "ZainteresiraniZaOglas" (
-  "stanarId" integer,
-  "oglasId" integer,
-  "poruka" varchar(500) NOT NULL,
-  PRIMARY KEY ("stanarId", "oglasId")
+                                         "korisnikId" integer,
+                                         "oglasId" integer,
+                                         "poruka" varchar(500) NOT NULL,
+                                         PRIMARY KEY ("korisnikId", "oglasId")
 );
 
 CREATE TABLE "Obavijest" (
-  "id" integer PRIMARY KEY,
-  "pošiljateljId" integer NOT NULL,
-  "vrstaObavijestId" integer NOT NULL,
-  "naslov" varchar(50) NOT NULL,
-  "poruka" varchar(200) NOT NULL,
-  "metaId" integer NOT NULL
+                             "id" integer PRIMARY KEY,
+                             "pošiljateljId" integer NOT NULL,
+                             "vrstaObavijestId" integer NOT NULL,
+                             "naslov" varchar(50) NOT NULL,
+                             "poruka" varchar(200) NOT NULL,
+                             "metaId" integer UNIQUE NOT NULL
 );
 
 CREATE TABLE "PoslanaObavijest" (
-  "kućanstvoId" integer,
-  "obavijestId" integer,
-  PRIMARY KEY ("kućanstvoId", "obavijestId")
+                                    "kućanstvoId" integer,
+                                    "obavijestId" integer,
+                                    PRIMARY KEY ("kućanstvoId", "obavijestId")
 );
 
 CREATE TABLE "VrstaObavijest" (
-  "id" integer PRIMARY KEY,
-  "vrsta" varchar(20) UNIQUE NOT NULL,
-  "prioritet" integer UNIQUE NOT NULL,
-  "metaId" integer NOT NULL
+                                  "id" integer PRIMARY KEY,
+                                  "vrsta" varchar(20) UNIQUE NOT NULL,
+                                  "prioritet" integer UNIQUE NOT NULL,
+                                  "metaId" integer UNIQUE NOT NULL
 );
 
 CREATE TABLE "Prostorija" (
-  "id" integer PRIMARY KEY,
-  "kućanstvoId" integer NOT NULL,
-  "vrstaProstorija" integer NOT NULL,
-  "kvadratura" decimal NOT NULL,
-  "metaId" integer NOT NULL
+                              "id" integer PRIMARY KEY,
+                              "kućanstvoId" integer NOT NULL,
+                              "vrstaProstorija" integer NOT NULL,
+                              "kvadratura" decimal NOT NULL,
+                              "metaId" integer UNIQUE NOT NULL
 );
 
 CREATE TABLE "VrstaProstorija" (
-  "id" integer PRIMARY KEY,
-  "vrsta" varchar(50) UNIQUE NOT NULL,
-  "metaId" integer NOT NULL
+                                   "id" integer PRIMARY KEY,
+                                   "vrsta" varchar(50) UNIQUE NOT NULL,
+                                   "metaId" integer NOT NULL
 );
 
 CREATE TABLE "Meta" (
-  "id" integer PRIMARY KEY,
-  "stvorioId" integer NOT NULL,
-  "uredioId" integer NOT NULL,
-  "stvorenoU" datetime NOT NULL,
-  "uređenoU" datetime NOT NULL,
-  "obrisano" bool NOT NULL DEFAULT false
+                        "id" integer PRIMARY KEY,
+                        "stvorioId" integer NOT NULL,
+                        "uredioId" integer NOT NULL,
+                        "stvorenoU" timestamp NOT NULL,
+                        "uređenoU" timestamp NOT NULL,
+                        "obrisano" bool NOT NULL DEFAULT false
 );
-
-COMMENT ON TABLE "Ulaz" IS 'ulicaId i ulicaBroj su skupa unique';
-
-COMMENT ON COLUMN "Ulaz"."predstavnikId" IS 'Nova zgrada, nitko ne živi još uuntra, nema predstavnika';
-
-COMMENT ON TABLE "Kompanija" IS 'ulicaId i ulicaBroj unique skupa';
-
-COMMENT ON TABLE "Korisnik" IS 'Osoba';
-
-COMMENT ON TABLE "PrijavljenaKućanstva" IS 'kId i pId ključ';
-
-COMMENT ON TABLE "ZainteresiraniZaOglas" IS 'sId i oId ključ';
-
-COMMENT ON TABLE "PoslanaObavijest" IS 'kId i oId su ključ';
 
 ALTER TABLE "Mjesto" ADD FOREIGN KEY ("državaId") REFERENCES "Država" ("id");
 
@@ -262,7 +251,7 @@ ALTER TABLE "Oglas" ADD FOREIGN KEY ("id") REFERENCES "Kućanstvo" ("id");
 
 ALTER TABLE "ZainteresiraniZaOglas" ADD FOREIGN KEY ("oglasId") REFERENCES "Oglas" ("id");
 
-ALTER TABLE "ZainteresiraniZaOglas" ADD FOREIGN KEY ("stanarId") REFERENCES "Stanar" ("id");
+ALTER TABLE "ZainteresiraniZaOglas" ADD FOREIGN KEY ("korisnikId") REFERENCES "Korisnik" ("id");
 
 ALTER TABLE "Obavijest" ADD FOREIGN KEY ("pošiljateljId") REFERENCES "Kućanstvo" ("id");
 
@@ -276,38 +265,40 @@ ALTER TABLE "Prostorija" ADD FOREIGN KEY ("vrstaProstorija") REFERENCES "VrstaPr
 
 ALTER TABLE "Prostorija" ADD FOREIGN KEY ("kućanstvoId") REFERENCES "Kućanstvo" ("id");
 
-ALTER TABLE "Meta" ADD FOREIGN KEY ("id") REFERENCES "Država" ("metaId");
+ALTER TABLE "Država" ADD FOREIGN KEY ("metaId") REFERENCES "Meta" ("id");
 
-ALTER TABLE "Meta" ADD FOREIGN KEY ("id") REFERENCES "Mjesto" ("metaId");
+ALTER TABLE "Mjesto" ADD FOREIGN KEY ("metaId") REFERENCES "Meta" ("id");
 
-ALTER TABLE "Meta" ADD FOREIGN KEY ("id") REFERENCES "Ulica" ("metaId");
+ALTER TABLE "Ulica" ADD FOREIGN KEY ("metaId") REFERENCES "Meta" ("id");
 
-ALTER TABLE "Meta" ADD FOREIGN KEY ("id") REFERENCES "Ulaz" ("metaId");
+ALTER TABLE "Ulaz" ADD FOREIGN KEY ("metaId") REFERENCES "Meta" ("id");
 
-ALTER TABLE "Meta" ADD FOREIGN KEY ("id") REFERENCES "Zgrada" ("metaId");
+ALTER TABLE "Zgrada" ADD FOREIGN KEY ("metaId") REFERENCES "Meta" ("id");
 
-ALTER TABLE "Meta" ADD FOREIGN KEY ("id") REFERENCES "Ugovor" ("metaId");
+ALTER TABLE "Ugovor" ADD FOREIGN KEY ("metaId") REFERENCES "Meta" ("id");
 
-ALTER TABLE "Meta" ADD FOREIGN KEY ("id") REFERENCES "Kompanija" ("metaId");
+ALTER TABLE "Kompanija" ADD FOREIGN KEY ("metaId") REFERENCES "Meta" ("id");
 
-ALTER TABLE "Meta" ADD FOREIGN KEY ("id") REFERENCES "NalogPlacanja" ("metaId");
+ALTER TABLE "NalogPlacanja" ADD FOREIGN KEY ("metaId") REFERENCES "Meta" ("id");
 
-ALTER TABLE "Meta" ADD FOREIGN KEY ("id") REFERENCES "Pričuva" ("metaId");
+ALTER TABLE "Pričuva" ADD FOREIGN KEY ("metaId") REFERENCES "Meta" ("id");
 
-ALTER TABLE "Meta" ADD FOREIGN KEY ("id") REFERENCES "Kućanstvo" ("metaId");
+ALTER TABLE "Kućanstvo" ADD FOREIGN KEY ("metaId") REFERENCES "Meta" ("id");
 
-ALTER TABLE "Meta" ADD FOREIGN KEY ("id") REFERENCES "Kontakt" ("metaId");
+ALTER TABLE "Kontakt" ADD FOREIGN KEY ("metaId") REFERENCES "Meta" ("id");
 
-ALTER TABLE "Meta" ADD FOREIGN KEY ("id") REFERENCES "VrstaKontakt" ("metaId");
+ALTER TABLE "VrstaKontakt" ADD FOREIGN KEY ("metaId") REFERENCES "Meta" ("id");
 
-ALTER TABLE "Meta" ADD FOREIGN KEY ("id") REFERENCES "Obavijest" ("metaId");
+ALTER TABLE "Obavijest" ADD FOREIGN KEY ("metaId") REFERENCES "Meta" ("id");
 
-ALTER TABLE "Meta" ADD FOREIGN KEY ("id") REFERENCES "VrstaObavijest" ("metaId");
+ALTER TABLE "VrstaObavijest" ADD FOREIGN KEY ("metaId") REFERENCES "Meta" ("id");
 
-ALTER TABLE "Meta" ADD FOREIGN KEY ("id") REFERENCES "Prostorija" ("metaId");
+ALTER TABLE "Prostorija" ADD FOREIGN KEY ("metaId") REFERENCES "Meta" ("id");
 
-ALTER TABLE "Meta" ADD FOREIGN KEY ("id") REFERENCES "VrstaProstorija" ("metaId");
+ALTER TABLE "VrstaProstorija" ADD FOREIGN KEY ("metaId") REFERENCES "Meta" ("id");
 
-ALTER TABLE "Meta" ADD FOREIGN KEY ("id") REFERENCES "Pritužba" ("metaId");
+ALTER TABLE "Pritužba" ADD FOREIGN KEY ("metaId") REFERENCES "Meta" ("id");
 
-ALTER TABLE "Meta" ADD FOREIGN KEY ("id") REFERENCES "Korisnik" ("metaId");
+ALTER TABLE "Korisnik" ADD FOREIGN KEY ("metaId") REFERENCES "Meta" ("id");
+
+ALTER TABLE "Oglas" ADD FOREIGN KEY ("metaId") REFERENCES "Meta" ("id");
