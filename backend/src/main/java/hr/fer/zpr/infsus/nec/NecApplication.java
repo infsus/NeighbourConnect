55f33da59cabcb67f25f2fadda08a740aa24d926
Karlito16 <karlo.dimjasevic@gmail.com>
2024-04-17 20:40:09 +0200

    Initial spring boot project generation

---
+++ backend/src/main/java/hr/fer/zpr/infsus/nec/NecApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.zpr.infsus.nec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NecApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/hr/fer/zpr/infsus/nec/NecApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.zpr.infsus.nec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
