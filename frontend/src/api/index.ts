270c7d4c0271b608bf288e8535206c2fb836416c
Karlito16 <karlo.dimjasevic@gmail.com>
2024-05-26 22:46:06 +0200

    Building entrance API delete

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,9 +1,11 @@
 import { auth } from "./auth";
 import { buildings } from "./buildings";
 import { streets } from "./streets";
+import { buildingEntrances } from "./buildingEntrances";
 
 export const api = {
     auth,
     buildings,
-    streets
+    streets,
+    buildingEntrances
 };
